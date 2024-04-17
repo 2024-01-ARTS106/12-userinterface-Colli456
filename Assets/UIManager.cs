cae1a6fada50342c1661f4572e374eb65494ce7a
Colli456 <72905402+Colli456@users.noreply.github.com>
2024-04-17 11:14:03 -0400

    Triggering Buttons Animation From the Script

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+
 public class UIManager : MonoBehaviour
 {
+    public Animator startButton;
+    public Animator settingsButton;
+
     // Start is called before the first frame update
 
     public void StartGame() 
@@ -12,7 +17,13 @@ public class UIManager : MonoBehaviour
         SceneManager.LoadScene("RocketMouse");
     }
 
+    public void OpenSettings()
+    {
+        startButton.SetBool("isHidden", true);
+        settingsButton.SetBool("isHidden", true);
+    }
+
     // Update is called once per frame
-   
-    
+
+
 }
